6cdaa0006144667704d11abe0ece346a57f1ac19
atchara <59160834@go.buu.ac.th>
2019-10-05 16:23:16 +0700

    async&await GET PUT POST

--- pokemons/pokemon.js
+++ pokemons/pokemon.js
@@ -1,3 +1,11 @@
+const mongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
+
+const DB_URL = 'mongodb+srv://59160834:59160834@pokemon-cluster-b880k.gcp.mongodb.net/admin?retryWrites=true&w=majority'
+const DB_NAME = 'example'
+const options = {useNewUrlParser: true, useUnifiedTopology: true }
+
+var collection, database, client
 let pokemons = []
 mockPokemon()
 
@@ -7,41 +15,124 @@ function Pokemon(name, type) {
     this.id = null
     this.type2 = null
 }
+/*------------------------------------------------------------------------------------------------------- */
 
-function savePokemon(name, type) {
-    let p = createPokemon(name, type)
-    pokemons.push(p)
-    return true
+/*------------------------------------------------------------------------------------------------------- */
+async function connectDatabase(){
+    if(client !== undefined && client !== null && client.isConnected){
+        return client
+    }
+    client = await mongoClient.connect(DB_URL, options).catch(err => console.error(err))
+    return client
 }
+/*------------------------------------------------------------------------------------------------------- */
+async function getCollection(name){
+    client = await connectDatabase().catch(err => console.error(err))
+    database = client.db(DB_NAME)
+    collection = database.collection(name)
+    return collection
+}
+/*------------------------------------------------------------------------------------------------------- */
+async function getPokemons(){
+    var collection = await getCollection('pokemons')
+    // var client = await mongoClient.connect(DB_URL, options).catch( err => console.log(err)) 
+    // var collection, database
+    // database = client.db(DB_NAME)
+    // collection = database.collection('pokemons')
+    try{
+        var result = await collection.find({}).toArray() 
+        return result
+    } catch(err) {
+        console.error(err)
+        return false
+    } finally {
+        client.close()
+    }
+}
+/*--------------------------------------------------------------------------------------------------------*/
+async function savePokemon(name, type) { //fn ที่เป็น sync ถือเป็น promise
+    //pokemons.push(p)
 
+    //callback --> m.connect(url, options, callback_fn) รับทั้งหมด 3 params
+    //function conectMongo (err,client) {   } --> callbackfn
+    
+    /*promise -- > m.connect().then((err, client) => { //มีแค่สำเร็วจกับไม่สำเร็จ
+        //สำเร็จทำตรงนี้ --> Resolved
+        collection.insert().then((result) => {  
+        })
+    }).catch((err) => {
+        //ไม่สำเร็จทำตรงนี้ --> Rejected
+    })*/
+    
+    //ไม่ค่อยใช้ promise ใช้ async await แทน
+    let p = createPokemon(name, type)
+    // var collection, database
+    // var client = await mongoClient.connect(DB_URL, options).catch( err => console.log(err)) //wait return result 
+    // database = client.db(DB_NAME)
+    // collection = database.collection('pokemons')
+    var collection = await getCollection('pokemons')
+    try{
+        var result = await collection.insert(p) //การทำงานแบบ async --> ใช้ await เพื่อรอรีเทิน
+        return true
+    } catch(err) {
+        console.error(err)
+        return false
+    } finally {
+        client.close()
+    }
+}
+/*------------------------------------------------------------------------------------------------------- */
 function createPokemon(name, type) {
     let p = new Pokemon(name, type)
     p.id = generateNewId(pokemons.length)
     return p
 }
-
+/*------------------------------------------------------------------------------------------------------- */
 function mockPokemon() {
     pokemons.push(createPokemon('Pikachu', 'Electric'))
     pokemons.push(createPokemon('Paras', 'Bug'))
 }
-
+/*------------------------------------------------------------------------------------------------------- */
 function generateNewId(num) {
     return num + 1
 }
-
+/*------------------------------------------------------------------------------------------------------- */
 function isPokemonExisted(id) {
     return pokemons[id-1] !== undefined && pokemons[id-1] !== null
 }
-
-function getPokemon(id) {
-    return pokemons[id - 1]
+/*------------------------------------------------------------------------------------------------------- */
+async function getPokemon(id) {
+    var collection = await getCollection('pokemons')
+try{
+    var result = await collection.findOne({_id: ObjectID(id)})
+    return result 
+} catch(err){
+    console.err(err)
+    return err
+} finally{
+    client.close()
+    }
 }
+/*------------------------------------------------------------------------------------------------------- */
+async function update(pokemon) {
+    var collection = await getCollection('pokemons')
+    try{
+        var result = await collection.updateOne({_id : ObjectID(id)},{$set:{"type2":type}})
+        return true
+        
+    } catch(err){
+        console.err(err)
+        return err
+    } finally{
+        client.close()
+    }
 
-function update(pokemon) {
-    pokemons[pokemon.id - 1] = pokemon
-    return true
+    // pokemons[pokemon.id - 1] = pokemon
+    // return true
 }
+/*------------------------------------------------------------------------------------------------------- */
 
+module.exports.getPokemons = getPokemons
 module.exports.isPokemonExisted = isPokemonExisted
 module.exports.savePokemon = savePokemon
 module.exports.getPokemon = getPokemon
--- pokemons/router.js
+++ pokemons/router.js
@@ -6,10 +6,16 @@ const pokemon = require('./pokemon')
 function isSufficientParam(v) {
     return v !== undefined && v !== null && v !== ''
 }
-
+/*------------------------------------------------------------------------------------------------------- */
 // GET /pokemons -> list all pokemons http://localhost:3000/pokemons
-router.get('/pokemons', (req, res) => res.send(pokemons))
-
+router.get('/pokemons', (req, res) => {
+    pokemon.getPokemons().then((result)=> {
+        res.status(200).send(result)
+    }).catch((err) => {
+        res.status(404).send({ error : 'Pokemons are Undefind'})
+    })
+})
+/*------------------------------------------------------------------------------------------------------- */
 // POST /pokemons -> add pokemon to list
 router.post('/pokemons', (req, res) => {
     if (!isSufficientParam(req.body.name)
@@ -19,34 +25,44 @@ router.post('/pokemons', (req, res) => {
         return
     }
 
-    let success = pokemon.savePokemon(req.body.name, req.body.type)
-    if (!success) {
-        res.status(400).send({ error: 'Create pokemon is unsuccessfully:' })
-        return
-    }
-    res.sendStatus(201)
+    pokemon.savePokemon(req.body.name, req.body.type).then((result) => {
+        res.sendStatus(201) //Resolved
+    }).catch((err)=> {
+        res.status(400).send({ error : 'create pokemon is unsuccessfully'}) //Reject
+    })
+    //let success = pokemon.savePokemon(req.body.name, req.body.type)
+    // if (!success) {
+    //     res.status(400).send({ error: 'Create pokemon is unsuccessfully:' })
+    //     return
+    // }
+    // res.sendStatus(201)
 })
-
+/*------------------------------------------------------------------------------------------------------- */
 // GET http://localhost:3000/pokemon/1
 router.get('/pokemon/:id', (req, res) => {
     if (!isSufficientParam(req.params.id)) {
         res.status(400).send({ error: 'Insufficient paramsters: id is required parameter' })
         return
     }
+    // let id = req.params.id
+    // let p = pokemon.getPokemon(id)
+    // if (p === undefined || p === null) {
+    //     res.status(400).send({ error: 'The pokemon could not be found' })
+    //     return
+    // }
 
-    let id = req.params.id
-    let p = pokemon.getPokemon(id)
-    if (p === undefined || p === null) {
-        res.status(400).send({ error: 'The pokemon could not be found' })
-        return
-    }
-
-    res.send(p)
+    
+    pokemon.getPokemon(req.params.id).then((result)=> {
+        res.status(200).send(result)
+    }).catch((err) => {
+        res.status(404).send({ error : 'Pokemons are Undefind'})
+    })
+    // res.send(p)
 })
-
+/*------------------------------------------------------------------------------------------------------- */
 // PUT http://localhost:3000/pokemon/1
 // Add type2
-router.put('/pokemon/:id', (req, res) => {
+router.put('/pokemon/:id', async (req, res) => {
     if (!isSufficientParam(req.body.type2)) {
         res.status(400).send({ error: 'Insufficient parameters: type2 is required parameter' })
         return
@@ -58,22 +74,30 @@ router.put('/pokemon/:id', (req, res) => {
     }
 
     let id = req.params.id
-    if (!pokemon.isPokemonExisted(id)) {
-        res.status(400).send({ error: 'Cannot update pokemon: Pokemon is not found' })
-        return
-    }
+    // if (!pokemon.isPokemonExisted(id)) {
+    //     res.status(400).send({ error: 'Cannot update pokemon: Pokemon is not found' })
+    //     return
+    // }
 
-    let p = pokemon.getPokemon(id)
-    p.type2 = req.body.type2
-    let success = pokemon.update(p)
-    if (!success) {
-        res.status(500).send({ error: 'Update pokemon is unsuccessfully' })
-        return
-    }
+    pokemon.update(req.params.id,req.body.type2).then((result)=> {
+        req.sendStatus(200)
+    }).catch((err)=> {
+        res.status(400).send({ error: 'Pokemon is not undefined'})
+    })
 
-    res.sendStatus(200)
+    // pokemon.update()
+
+    // let p = pokemon.getPokemon(id)
+    // p.type2 = req.body.type2
+    // let success = pokemon.update(p)
+    // if (!success) {
+    //     res.status(500).send({ error: 'Update pokemon is unsuccessfully' })
+    //     return
+    // }
+
+    // res.sendStatus(200)
 })
-
+/*------------------------------------------------------------------------------------------------------- */
 router.delete('/pokemon/:id', (req, res) => {
     if (!isSufficientParam(req.params.id)) {
         res.status(400).send({ error: 'Insufficient parameters: id is required parameter' })
